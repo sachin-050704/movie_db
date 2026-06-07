948b9193130e05711db1c66f90031d6b752a598e
Sachin Kanojiya <sachinkanojiya1810@gmail.com>
2026-06-07 19:04:06 +0530

    Configure Django for Render

--- movieDB/settings.py
+++ movieDB/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j$t@&y(pva5kj!yeq=0h!p-byblpiv(gldn%px##2g5#zjk!ag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
