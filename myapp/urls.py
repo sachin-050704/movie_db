cf7205ec9308b991bb4ac34bb48d347f6d09385b
Sachin Kanojiya <sachinkanojiya1810@gmail.com>
2026-06-07 19:57:06 +0530

    Test deploy

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from .views import *
 from rest_framework.routers import DefaultRouter
+from django.http import HttpResponse
 
 router = DefaultRouter()
 router.register("users", AdminUserViewSet)
@@ -10,6 +11,7 @@ urlpatterns = [
     path('userList/', user_list, name='user_list'),
     path('updateUser/<str:username>', update_user, name='update_user'),
     path('profile/', profile, name='profile'),
-    path("check-media/", check_media),
+    path("checkmedia/", check_media),
+    path("test123/", lambda request: HttpResponse("working"))
     path("admin1/", include(router.urls)),
 ]
