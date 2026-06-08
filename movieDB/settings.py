a746fe9ad08df733f728f6d387136cee1f00f5e4
Sachin Kanojiya <sachinkanojiya1810@gmail.com>
2026-06-08 12:41:25 +0530

    Switch database to PostgreSQL

--- movieDB/settings.py
+++ movieDB/settings.py
@@ -89,12 +89,13 @@ WSGI_APPLICATION = 'movieDB.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
